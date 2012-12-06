52b3655893369ab7e68873910a7c3404c4b87762
Andrey Bayandin <cukune@gmail.com>
2012-12-06 12:36:55 +0600

    x

--- js/ie-lt-8.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.b-text ul li').prepend('<div class="li-ie-before">→</div>');
-
-    $('.b-text ol li').each(function(i){
-        var i = i+1;
-        $(this).prepend('<span class="li-ie-before">'+i+'. </span>')
-    });
-});
